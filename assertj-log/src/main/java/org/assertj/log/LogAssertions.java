b7230978fba451fa284e1e78dd5248449b07dc01
Fabien DUMINY <fduminy@jnode.org>
2016-03-06 21:29:22 +0100

    Add tests for LogAssertions.

--- assertj-log/src/main/java/org/assertj/log/LogAssertions.java
+++ assertj-log/src/main/java/org/assertj/log/LogAssertions.java
@@ -1,18 +1,20 @@
 /**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * <p>
+ *
  * Copyright 2012-2015 the original author or authors.
  */
 package org.assertj.log;
 
 import org.assertj.core.api.Assertions;
+
+import java.util.Objects;
 
 /**
  * Entry point for assertion methods on logs.
@@ -20,7 +22,8 @@ import org.assertj.core.api.Assertions;
  * @author Fabien DUMINY
  */
 public class LogAssertions extends Assertions {
-    public static LogAssert assertThat(LogAssertRule logs) {
-        return new LogAssert(logs);
+    public static LogAssert assertThat(LogAssertRule logAssertRule) {
+        Objects.requireNonNull(logAssertRule, "logAssertRule");
+        return new LogAssert(logAssertRule);
     }
 }
---
+++ assertj-log/src/test/java/org/assertj/log/LogAssertionsTest.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * <p>
+ * Copyright 2012-2015 the original author or authors.
+ */
+package org.assertj.log;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for class {@link LogAssertionsTest}.
+ *
+ * @author Fabien DUMINY
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LogAssertionsTest {
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    @Mock
+    private LogAssertRule logs;
+
+    @Test
+    public void testAssertThat() throws Exception {
+        LogAssert logAssert = LogAssertions.assertThat(logs);
+
+        assertThat(logAssert).isNotNull();
+        verify(logs).getEvents();
+    }
+
+    @Test
+    public void testAssertThat_nullRule() throws Exception {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("logAssertRule");
+
+        LogAssertions.assertThat((LogAssertRule) null);
+    }
+}
